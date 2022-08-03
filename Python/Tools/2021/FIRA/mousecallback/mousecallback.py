d5defd48976f642d1addf176ccd14de99539f3af
ye_geon_hui <dprjsgml@gmail.com>
2022-08-04 00:21:28 +0900

    update mousecallback

--- Python/Tools/2021/FIRA/mousecallback/mousecallback.py
+++ Python/Tools/2021/FIRA/mousecallback/mousecallback.py
@@ -1,4 +1,3 @@
-import numpy as np
 import cv2
 import os
 
@@ -11,14 +10,19 @@ def draw(event, x, y, flags, param):
 
         elif event == cv2.EVENT_LBUTTONUP:
             cv2.line(image, (ix, iy), (x, y), (255, 255, 255), 2)
+            cv2.imshow("image", image)
     else:
         if event == cv2.EVENT_LBUTTONDOWN:
             cv2.circle(image, (x, y), radius, (255, 255, 255), 2)
+            cv2.imshow("image", image)
+        # 스크롤 업하면 원 크기가 증가
+        # 스크롤 다운하면 원 크기 감소
         elif event == cv2.EVENT_MOUSEWHEEL:
             if flags > 0:
                 radius += 1
             elif radius > 1:
                 radius -= 1
+
 
 radius = 3
 ix, iy = (-1, -1)
@@ -27,14 +31,18 @@ for idx, (root, dirs, files) in enumerate(os.walk("Image")):
     Image_list = [img for img in files if img.lower().endswith(".jpg")]
     for img in Image_list:
         image = cv2.imread(os.path.join(root, img))
+        image = cv2.resize(image, (500, 600))
+        cv2.imshow("image", image)
+        cv2.setMouseCallback("image", draw, image)
         while True:
-            cv2.imshow("image", image)
-            cv2.setMouseCallback("image", draw, image)
-            
-            key = cv2.waitKey(1)
+            # 키보드 입력 무한대기 
+            key = cv2.waitKey(0) 
+
+            # m을 누르면 모드변경(라인 -> 원)
             if key == ord("m"):
                 mode = not mode
             elif key == 27:
                 break 
-            
+            elif cv2.waitKey(0):
+                break
         cv2.destroyAllWindows()
