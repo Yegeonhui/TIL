e0072b49efde56cc5d4c83974d3b697601ba429e
ye_geon_hui <dprjsgml@gmail.com>
2022-08-05 00:03:11 +0900

    no message

--- Python/Tools/2021/PySimpleGUI/MakeTable/MakeTable.py
+++ Python/Tools/2021/PySimpleGUI/MakeTable/MakeTable.py
@@ -2,13 +2,13 @@ import PySimpleGUI as sg
 import random
 import string
 
-# 데이터 word()는 컬럼헤더, number()는 랜덤 데이터 1000개, make_talbe()함수는 테이블에 들어갈 데이터셋을 만든다.
-
 def word():
     return ''.join(random.choice(string.ascii_lowercase) for i in range(10))
 
+
 def number(max_val = 1000):
     return random.randint(0, max_val)
+
 
 def make_table(num_rows, num_cols):
     data = [[j for j in range(num_cols)] for i in range(num_rows)]
@@ -17,7 +17,11 @@ def make_table(num_rows, num_cols):
         data[i] = [word(), *[number() for i in range(num_cols - 1)]]
     return data
 
+
+# 행 15, 열 6 데이터 생성
 data = make_table(num_rows = 15, num_cols = 6)
+
+# heading 생성
 headings = [str(data[0][x]) for x in range(len(data[0]))]
 
 # 테이블
@@ -32,7 +36,9 @@ layout = [
                     num_rows = 20,
                     alternating_row_color = 'lightyellow',
                     key = '-TABLE-',
-                    tooltip = 'This is a table')]
+                    tooltip = 'This is a table')],
+            [sg.Button('Double'), sg.Button('Change Colors')]
+            
           ]
 window = sg.Window('The Table Element', layout)
 
@@ -41,10 +47,14 @@ while True:
     print(event, values)
     if event is None:
         break
+    
+    # 현재의 데이터를 그대로 다시 추가 
     if event == 'Double':
         for i in range(len(data)):
             data.append(data[i])
         window['-TABLE-'].update(values = data)
+    
+    # 8행과 9행의 색을 변경
     elif event == 'Change Colors':
         window['-TABLE-'].update(row_colors = ((8, 'white', 'red'), (9, 'green')))
 
--- Python/Tools/2021/PySimpleGUI/readme.md
+++ Python/Tools/2021/PySimpleGUI/readme.md
@@ -1,19 +1,14 @@
-# Stitching
-> 여러장의 이미지를 하나로 합치는 코드
-
-## What is Stitching?
-1. 동일 장면의 사진을 자연스럽게 붙여서 한장의 사진으로 만드는 기술이다.
-2. 기본적인 방법은 여러장의 영상에서 특징점을 검출하고 특징점이 동일한 것들을 찾아서 두 장의 영상과의 투시변환 관계를 찾아내어 이어붙인다.
+# SimpleGUI
+> Python으로 간편하게 GUI를 만들수 있는 라이브러리 연습
 
 ## How to use?
-1. step1_CameraCalibration.py
-- 카메라의 왜곡을 펴주는 코드 
-2. step2_Stitching.py
+1. MakeTable.py
+- table을 랜덤으로 생성하고,
+- 데이터를 그대로 다시 추가
+- 색 변경 기능 추가2. step2_Stitching.py
 - 왜곡이 없어진 이미지를 합쳐주는 코드
-
+2. 
 ## install Library
 ```
-pip install opencv-python
-pip install glob2
-pip install numpy
+pip install PySimpleGUI
 ```
