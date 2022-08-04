44f4853c9a2d179a2e2a0c200f8a872d77dec262
ye_geon_hui <dprjsgml@gmail.com>
2022-08-05 00:12:11 +0900

    update thread

--- Python/Tools/2021/PySimpleGUI/Thread/Thread.py
+++ Python/Tools/2021/PySimpleGUI/Thread/Thread.py
@@ -8,7 +8,7 @@ import PySimpleGUI as sg
 def worker_thread(thread_name, run_freq, gui_queue):
     print("starting thread 1 - {} that runs every {} ms".format(thread_name, run_freq))
     for i in itertools.count():
-        time.sleep(run_freq / 100)
+        time.sleep(run_freq / 1000)
         gui_queue.put('{} - {}'.format(thread_name, i))
         
 def the_main_gui(gui_queue):
@@ -37,14 +37,11 @@ def the_main_gui(gui_queue):
     window.close()
 
 def main():
+    # Queue 생성
     gui_queue = queue.Queue()
     gui_queue.put(1)
-    threading.Thread(target = worker_thread, args = ('Thread 1', 500, gui_queue), daemon = True).start()
-    #threading.Thread(target = worker_thread, args = ('Thread 2', 500, gui_queue), daemon = True).start()
+    threading.Thread(target=worker_thread, args=('Thread 1', 500, gui_queue), daemon=True).start()
     the_main_gui(gui_queue)
-
-
-
 
 if __name__ == "__main__":
     main()
--- Python/Tools/2021/PySimpleGUI/readme.md
+++ Python/Tools/2021/PySimpleGUI/readme.md
@@ -1,14 +1,8 @@
 # SimpleGUI
 > Python으로 간편하게 GUI를 만들수 있는 라이브러리 연습
 
-## How to use?
-1. MakeTable.py
-- table을 랜덤으로 생성하고,
-- 데이터를 그대로 다시 추가
-- 색 변경 기능 추가2. step2_Stitching.py
-- 왜곡이 없어진 이미지를 합쳐주는 코드
-2. 
-## install Library
-```
-pip install PySimpleGUI
-```
+1. MakeTable.
+2. Slider_progressbar
+3. Thread
+
+
