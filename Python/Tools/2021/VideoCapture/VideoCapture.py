80b5b54e618096a76805fb12c80f4c78a03c4fb1
ye_geon_hui <dprjsgml@gmail.com>
2022-08-04 23:29:59 +0900

    Revert "update videocapture"
    This reverts commit fe8c2064ed72063f411abd3359b18e18c6267925.

--- Python/Tools/2021/VideoCapture/VideoCapture.py
+++ Python/Tools/2021/VideoCapture/VideoCapture.py
@@ -1,41 +1,36 @@
 import cv2
 import os
 
-start_second = 10
-cut_time = 3
-
 os.makedirs(os.getcwd() + "/frame", exist_ok = True)
 for idx, (root, dirs, files) in enumerate(os.walk("video")):
     Video_list = [video for video in files if video.lower().endswith(".mp4")]
     for video in Video_list:
-        name = os.path.splitext(video)[0]
         cap = cv2.VideoCapture(os.path.join(root, video))
-
-        # fps 
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        print(fps)
-        
-        # 동영상 총 프레임
-        total_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        
-        # 지정 초 부터 시작
-        cap.set(cv2.CAP_PROP_POS_FRAMES, start_second * fps)
+        print(cap.get(cv2.CAP_PROP_FPS))
+        print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = 30
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 300)
         
         while True:
             frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
             print(frame)
             
             success, image = cap.read()
+            #총 프레임 수 
             
             if cap.get(cv2.CAP_PROP_FRAME_COUNT) == frame :
                 break 
             
-            if frame % (fps * cut_time) == 0:
+            if frame % (fps * 3) == 0:
                 if success == False:
                     print("프레임 추출 실패") 
+                if os.path.split(root)[1] == "L":
+                    image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                elif os.path.split(root)[1] == "R":
+                    image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
                 try:
-                    cv2.imwrite("frame/" + name + "_" + str(frame / fps) + ".jpg", image)
-                    print("saved imaged" + name + "_" + str(frame / fps) + ".jpg")
+                    cv2.imwrite(os.getcwd() + "/frame/" + video + "_" + str(int(frame / fps / 3)) + ".jpg", image)
+                    print("saved imaged" + video + "_" + str(int(frame / fps / 3)) + ".jpg")
                 except:
                     print("오류")
             
